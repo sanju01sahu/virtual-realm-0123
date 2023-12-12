62ef6abe2ae6d21d55d82f23401e14e235ccf805
abii225 <abhayv225@gmail.com>
2023-12-12 21:18:46 +0530

    navbar done

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,95 @@
-import React from 'react'
+import React from "react";
+import { Box, Button, Flex, IconButton, Link, Spacer } from "@chakra-ui/react";
 
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from "@chakra-ui/react";
+import {
+  AddIcon,
+  EditIcon,
+  ExternalLinkIcon,
+  HamburgerIcon,
+  RepeatIcon,
+} from "@chakra-ui/icons";
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <Flex
+        w="100%"
+        bg={"#ECF9FF"}
+        h={"80px"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Box h={"40px"} ml={"10px"} name="logo" display={"flex"}>
+          <img
+            style={{ height: "100%" }}
+            src="https://i.ibb.co/THtf5pd/cookease.png"
+            alt="logo"
+          />
+          <Box
+            name="nav-links"
+            ml={"20px"}
+            w={{ base: "0%", md: "500px" }}
+            bg={""}
+            fontWeight={"700"}
+            fontSize={"18px"}
+            color={"green"}
+            justifyContent={"space-between"}
+            alignItems={"center"}
+            display={{ base: "none", lg: "flex" }}
+          >
+            <Link>Our Menu</Link>
+            <Link>How it Works</Link>
+            <Link>CookBook</Link>
+            <Link>About Us</Link>
+          </Box>
+        </Box>
 
-export default Navbar
+        <Box>
+          <Button
+            mr={"20px"}
+            bg={"#87CBB9"}
+            display={{ base: "none", lg: "flex" }}
+          >
+            Sign up
+          </Button>
+
+          <Menu>
+            <MenuButton
+              display={{ base: "", lg: "none" }}
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+              mr={"20px"}
+            />
+            <MenuList>
+              <MenuItem icon={<AddIcon />} command="">
+                Our Menu
+              </MenuItem>
+              <MenuItem icon={<ExternalLinkIcon />} command="">
+                CookBook
+              </MenuItem>
+              <MenuItem icon={<RepeatIcon />} command="">
+                How it Works
+              </MenuItem>
+              <MenuItem icon={<EditIcon />} command="">
+                About Us
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+      </Flex>
+    </div>
+  );
+};
+
+export default Navbar;
