a4a312a082537f06847580e641f4b13e295a5f9b
Khelsai Rajwade <khelsai@gmail.com>
2023-12-13 23:18:03 +0530

    fw24_054-day-2

--- frontend/src/Routes/Allroutes.jsx
+++ frontend/src/Routes/Allroutes.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import Aboutus from "../pages/Aboutus";
 
 const Allroutes = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />}></Route>
+        <Route path="/aboutus" element={<Aboutus />}></Route>
+
       </Routes>
     </div>
   );
